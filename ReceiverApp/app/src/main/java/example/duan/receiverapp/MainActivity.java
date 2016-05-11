c56653fbe19955674e0a0b701846270abb15f061
duanbressan <duan.trabalho@hotmail.com>
2016-05-11 17:21:16 -0300

    Funções de receber string no segundo app

--- ReceiverApp/app/src/main/java/example/duan/receiverapp/MainActivity.java
+++ ReceiverApp/app/src/main/java/example/duan/receiverapp/MainActivity.java
@@ -1,13 +1,36 @@
 package example.duan.receiverapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textShared;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        textShared = (TextView) findViewById(R.id.textShared);
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
+                handleSendText(intent);
+            }
+        }
+    }
+
+    void handleSendText(Intent intent) {
+        String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+        if (sharedText != null) {
+            textShared.setText(sharedText);
+        }
     }
 }
