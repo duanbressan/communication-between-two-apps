744c28368be92053f1a764c71f1375da7cd818d1
duanbressan <duan.trabalho@hotmail.com>
2016-05-11 17:45:27 -0300

    EditText para o usuário digitar o texto e validação para o envio

--- sendingapp/app/src/main/java/example/duan/sendingapp/MainActivity.java
+++ sendingapp/app/src/main/java/example/duan/sendingapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,24 +15,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final EditText editText = (EditText) findViewById(R.id.editText);
         Button buttonSend = (Button) findViewById(R.id.buttonSend);
+
         buttonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                send();
+                if(editText.getText().toString().equals("")){
+                    Toast t = Toast.makeText(getApplicationContext(), "Adicione um texto!", Toast.LENGTH_SHORT);
+                    t.show();
+                }
+                else{
+                    send(editText.getText().toString());
+                }
             }
         });
     }
 
-    private void send(){
+    private void send(String text_to_send) {
         Intent sendIntent = new Intent();
         sendIntent.setClassName("example.duan.receiverapp", "example.duan.receiverapp.MainActivity");
         sendIntent.setAction(Intent.ACTION_SEND);
         sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        sendIntent.putExtra(Intent.EXTRA_TEXT, "Olá, tudo bem?");
+        sendIntent.putExtra(Intent.EXTRA_TEXT, text_to_send);
         sendIntent.setType("text/plain");
 
         startActivity(sendIntent);
-
     }
 }
