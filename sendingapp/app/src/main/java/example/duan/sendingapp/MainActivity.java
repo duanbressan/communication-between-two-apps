38ca9dcab04968604ec28dc32aa5121135c2eeec
duanbressan <duan.trabalho@hotmail.com>
2016-05-11 17:12:08 -0300

    Método de envio de string pronto

--- sendingapp/app/src/main/java/example/duan/sendingapp/MainActivity.java
+++ sendingapp/app/src/main/java/example/duan/sendingapp/MainActivity.java
@@ -1,7 +1,10 @@
 package example.duan.sendingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonSend = (Button) findViewById(R.id.buttonSend);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                send();
+            }
+        });
+    }
+
+    private void send(){
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "Olá, tudo bem?");
+        sendIntent.setType("text/plain");
+        startActivity(Intent.createChooser(sendIntent, "send to"));
+
     }
 }
